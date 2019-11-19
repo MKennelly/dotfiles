7bf7c848f92aad8fc6c7512165b5902a601ca428
MKennelly <mikewjkennelly@gmail.com>
2019-11-18 21:59:05 -0500

    Make fzf search all files including ignored ones as old flag wasn't working

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -23,7 +23,7 @@
 
 # Settings
 	export VISUAL=vim
-	export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
+	export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden'
 
 source ~/dotfiles/zsh/plugins/fixls.zsh
 

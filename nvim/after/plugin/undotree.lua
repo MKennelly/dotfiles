39de6324a238e581f52ade98b32aab2a833784d7
Mike Kennelly <mikewjkennelly@gmail.com>
2023-04-04 15:59:38 -0400

    Adds undodir config to persist undotree

--- nvim/after/plugin/undotree.lua
+++ nvim/after/plugin/undotree.lua
@@ -1,1 +1,14 @@
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+if vim.fn.has("persistent_undo") == 1 then
+   local target_path = vim.fn.expand('~/.undodir')
+   --  create the directory and any parent directories
+   --  if the location does not exist.
+   if vim.fn.isdirectory(target_path) == 0 then
+       vim.call(vim.fn.mkdir(target_path, "p", 0700))
+   end
+   vim.opt.undodir = target_path
+   vim.opt.undofile = true
+end
+
+vim.g.undotree_SetFocusWhenToggle = 1

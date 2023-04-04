0e781b33c23123b71caf80eec88320f8371918ef
Mike Kennelly <mikewjkennelly@gmail.com>
2023-04-04 16:19:01 -0400

    Adds keymappings for fugitive 3-way diff

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
--- " Vim Fugitive 3-way diff stuff
--- nnoremap <leader>gd :Gvdiff<CR>
--- nnoremap gdh :diffget //2<CR>
--- nnoremap gdl :diffget //3<CR>
+-- Vim Fugitive 3-way diff stuff
+-- Press dv in fugitive on file to vert split, then gh for taking left
+-- gl for taking right
+vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>")
+vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")

fa323cb6200d0101f69086f2d1a03d7bd3a2068c
Mike Kennelly <mikewjkennelly@gmail.com>
2023-04-20 22:46:07 -0400

    Make C-g grep search use fuzzy by default rather than regex

--- nvim/after/plugin/fzflua.lua
+++ nvim/after/plugin/fzflua.lua
@@ -2,4 +2,4 @@ local fzflua = require('fzf-lua')
 
 -- vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<C-p>', fzflua.files, {})
-vim.keymap.set('n', '<C-g>', fzflua.live_grep, {})
+vim.keymap.set('n', '<C-g>', function() fzflua.grep({ search = "" }) end, {})

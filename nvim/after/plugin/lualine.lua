0fbd848c4269ddd4ccd7af8e71f0f6cbd0376a66
Mike Kennelly <mikewjkennelly@gmail.com>
2023-04-21 17:13:47 -0400

    Show file path in lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,5 +1,21 @@
 require('lualine').setup({
 	options = {
 		theme = 'onedark'
+	},
+	sections = {
+		lualine_c = {
+			{
+				'filename',
+				path = 1
+			}
+		}
+	},
+	inactive_sections = {
+		lualine_c = {
+			{
+				'filename',
+				path = 1
+			}
+		}
 	}
 })

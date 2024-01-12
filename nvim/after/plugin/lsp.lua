9c50deb243d38ee15005658e85bd2e8b0b89cf03
Mike Kennelly <mikewjkennelly@gmail.com>
2024-01-12 12:49:10 -0500

    Enable tailwind lsp in CSS files

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -59,4 +59,8 @@ require('lspconfig').stylelint_lsp.setup({
   },
 })
 
+require('lspconfig').tailwindcss.setup({
+  filetypes = {'aspnetcorerazor', 'astro', 'astro-markdown', 'blade', 'clojure', 'css', 'django-html', 'htmldjango', 'edge', 'eelixir', 'elixir', 'ejs', 'erb', 'eruby', 'gohtml', 'gohtmltmpl', 'haml', 'handlebars', 'hbs', 'html', 'html-eex', 'heex', 'jade', 'leaf', 'liquid', 'markdown', 'mdx', 'mustache', 'njk', 'nunjucks', 'php', 'razor', 'slim', 'twig', 'css', 'less', 'postcss', 'sass', 'scss', 'stylus', 'sugarss', 'javascript', 'javascriptreact', 'reason', 'rescript', 'typescript', 'typescriptreact', 'vue', 'svelte'}
+})
+
 lsp.setup()

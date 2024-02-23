7fe9c72cc2705b2e0f1eef531f3db68035f2be2e
Mike Kennelly <mikewjkennelly@gmail.com>
2024-02-23 11:37:50 -0500

    Another oil remap fix

--- nvim/lua/mike/remap.lua
+++ nvim/lua/mike/remap.lua
@@ -50,10 +50,10 @@ map("v", "<leader>d", "\"_d")
 
 -- Pane/tab management
 -- map("n", "<leader>e", vim.cmd.Exp)
-map("n", "<leader>e", "<CMD>Oil<CR>")
+map("n", "<leader>e", vim.cmd.Oil)
 map("n", "<leader>t", function()
   vim.cmd.tabnew()
-  vim.cmd.Exp()
+  vim.cmd.Oil()
 end)
 map("n", "<leader>v", vim.cmd.vsplit)
 map("n", "<leader>s", vim.cmd.split)

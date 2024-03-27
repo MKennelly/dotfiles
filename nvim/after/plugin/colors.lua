9eab45ed4923835e582ba0c169fac1038e9c25a5
Mike Kennelly <mikewjkennelly@gmail.com>
2024-03-26 22:29:15 -0400

    Update color scheme to make diff highlighting more obvious

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -8,4 +8,15 @@
 --
 -- onedark.load()
 
+local darken = require("catppuccin.utils.colors").darken
+require("catppuccin").setup {
+    custom_highlights = function(colors)
+        return {
+          DiffAdd = { bg = darken(colors.green, 0.4, colors.base) },
+          DiffChange = { bg = darken(colors.blue, 0.3, colors.base) },
+          DiffDelete = { bg = darken(colors.red, 0.4, colors.base) },
+        }
+    end
+}
+
 vim.cmd.colorscheme "catppuccin-frappe"

0acae68f2085febc69aeedb83fe0c0528e13a799
Mike Kennelly <mikewjkennelly@gmail.com>
2024-11-25 16:34:11 -0500

    Switch copilot for supermaven

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -75,53 +75,65 @@ return {
   {'nvim-pack/nvim-spectre', dependencies = { {'nvim-lua/plenary.nvim'} } },
 
   -- 'github/copilot.vim',
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup({
+  --       -- panel = {
+  --         -- enabled = true,
+  --         -- auto_refresh = false,
+  --         -- keymap = {
+  --         --   jump_prev = "[[",
+  --         --   jump_next = "]]",
+  --         --   accept = "<CR>",
+  --         --   refresh = "gr",
+  --         --   open = "<M-CR>"
+  --         -- },
+  --         -- layout = {
+  --         --   position = "bottom", -- | top | left | right
+  --         --   ratio = 0.4
+  --         -- },
+  --       -- },
+  --       suggestion = {
+  --         enabled = true,
+  --         auto_trigger = true,
+  --         debounce = 75,
+  --         keymap = {
+  --           accept = "<M-y>",
+  --           accept_word = false,
+  --           accept_line = false,
+  --           next = "<M-n>",
+  --           prev = "<M-p>",
+  --           dismiss = "<C-]>",
+  --         },
+  --       },
+  --       -- filetypes = {
+  --         -- yaml = false,
+  --         -- markdown = false,
+  --         -- help = false,
+  --         -- gitcommit = false,
+  --         -- gitrebase = false,
+  --         -- hgcommit = false,
+  --         -- svn = false,
+  --         -- cvs = false,
+  --         -- ["."] = false,
+  --       -- },
+  --       -- copilot_node_command = 'node', -- Node.js version must be > 16.x
+  --       -- server_opts_overrides = {},
+  --     })
+  --   end,
+  -- },
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+    "supermaven-inc/supermaven-nvim",
     config = function()
-      require("copilot").setup({
-        -- panel = {
-          -- enabled = true,
-          -- auto_refresh = false,
-          -- keymap = {
-          --   jump_prev = "[[",
-          --   jump_next = "]]",
-          --   accept = "<CR>",
-          --   refresh = "gr",
-          --   open = "<M-CR>"
-          -- },
-          -- layout = {
-          --   position = "bottom", -- | top | left | right
-          --   ratio = 0.4
-          -- },
-        -- },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<M-y>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-n>",
-            prev = "<M-p>",
-            dismiss = "<C-]>",
-          },
-        },
-        -- filetypes = {
-          -- yaml = false,
-          -- markdown = false,
-          -- help = false,
-          -- gitcommit = false,
-          -- gitrebase = false,
-          -- hgcommit = false,
-          -- svn = false,
-          -- cvs = false,
-          -- ["."] = false,
-        -- },
-        -- copilot_node_command = 'node', -- Node.js version must be > 16.x
-        -- server_opts_overrides = {},
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<M-y>",
+          clear_suggestion = "<M-c>",
+          accept_word = "<M-j>",
+        }
       })
     end,
   },

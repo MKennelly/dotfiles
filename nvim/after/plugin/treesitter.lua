a711f6b230b27f4d0cc53e595b27df4e1b661e63
Mike Kennelly <mikewjkennelly@gmail.com>
2025-03-11 15:14:55 -0400

    Updates themes and treesitter config/deps

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -2,7 +2,25 @@
 require('ts_context_commentstring').setup { }
 vim.g.skip_ts_context_commentstring_module = true
 
-require'nvim-treesitter.configs'.setup {
+-- Use for automatically closing tags in html-like languages
+require('nvim-ts-autotag').setup({
+  opts = {
+    -- Defaults
+    enable_close = true, -- Auto close tags
+    enable_rename = true, -- Auto rename pairs of tags
+    enable_close_on_slash = false -- Auto close on trailing </
+  },
+  -- Also override individual filetype configs, these take priority.
+  -- Empty by default, useful if one of the "opts" global settings
+  -- doesn't work well in a specific filetype
+  -- per_filetype = {
+  --   ["html"] = {
+  --     enable_close = false
+  --   }
+  -- }
+})
+
+require'nvim-treesitter.configs'.setup({
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "html", "css", "query", "javascript", "typescript", "tsx", "astro", "svelte" },
 
@@ -21,16 +39,4 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-
-  -- Plugins
-  autotag = {
-	  enable = true,
-	  filetypes = {
-		  'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript', 'astro',
-		  'xml',
-		  'php',
-		  'markdown',
-		  'glimmer','handlebars','hbs'
-	  }
-  },
-}
+})
